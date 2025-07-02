f4c33ec0f5fbcf5be6b7e1c6b135b0b8c430fd12
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-07-02 15:52:44 +0600

    Deleted head from linked list

---
+++ deleteAtHead.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int integerValue;
+    Node *nextPointer;
+
+    Node(int integerValue)
+    {
+        this->integerValue = integerValue;
+        this->nextPointer = NULL;
+    }
+};
+
+void insertAtTail(Node *&head, Node *&tail, int integerValue)
+{
+    Node *newNode = new Node(integerValue);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        tail->nextPointer = newNode;
+        tail = newNode;
+    }
+}
+
+void deleteHead(Node *&head)
+{
+    Node *deleteNode = head;
+    head = head->nextPointer;
+    delete deleteNode;
+}
+
+void printLinkedList(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->integerValue << endl;
+        tmp = tmp->nextPointer;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int inputValue;
+    while (true)
+    {
+        cin >> inputValue;
+        if (inputValue == -1)
+        {
+            break;
+        }
+        else
+        {
+            insertAtTail(head, tail, inputValue);
+        }
+    }
+
+    deleteHead(head);
+    printLinkedList(head);
+
+    return 0;
+}
