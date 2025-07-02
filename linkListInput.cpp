9beba91bad736fdfd354c4e098ab2e508529fcd9
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-07-02 12:12:52 +0600

    Taken input a link list

---
+++ linkListInput.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int integerValue;
+    Node *nextPointer;
+
+    Node(int integerValue)
+    {
+        this->integerValue = integerValue;
+        this->nextPointer = NULL;
+    }
+};
+
+void insertAtTail(Node *&head, Node *&tail, int integerValue)
+{
+    Node *newNode = new Node(integerValue);
+    Node *tmp;
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        tmp = tail;
+        tmp->nextPointer = newNode;
+        tail = newNode;
+    }
+}
+
+void printLinkedList(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->integerValue << endl;
+        tmp = tmp->nextPointer;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int inputValue;
+    while (true)
+    {
+        cin >> inputValue;
+        if (inputValue == -1)
+        {
+            break;
+        }
+        else
+        {
+            insertAtTail(head, tail, inputValue);
+        }
+    }
+
+    printLinkedList(head);
+
+    return 0;
+}
